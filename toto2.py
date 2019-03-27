f05808b539f3d6d081c236c8249d4db23ab5426e
Martyna Łęcka <martynalecka.ml@gmail.com>
2019-03-28 00:55:34 +0100

    basic: exercises from python101.readthedocs.io

---
+++ toto2.py
@@ -0,0 +1,7 @@
+#!user/bin/env python3
+# -*- coding: utf-8 -*-
+
+numb = int(input("How many numbers will you want? "))
+maxNumb = int(input("Put max value of this number: "))
+
+print("Chose %s from %s numbers: " % (numb, maxNumb))
