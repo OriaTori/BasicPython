449582ec37dae3fd0102e17d226645327e802052
Martyna Łęcka <martynalecka.ml@gmail.com>
2019-03-29 01:25:11 +0100

    Few minutes with - 1.2.2

--- toto2.py
+++ toto2.py
@@ -1,7 +1,20 @@
 #!user/bin/env python3
 # -*- coding: utf-8 -*-
 
+import random
+
 numb = int(input("How many numbers will you want? "))
 maxNumb = int(input("Put max value of this number: "))
 
 print("Chose %s from %s numbers: " % (numb, maxNumb))
+
+listNumb = []
+#for i in range(numb):
+i = 0
+while i < numb:
+    n = random.randint(1, maxNumb)
+    if listNumb.count(n) == 0:
+        listNumb.append(n)
+        i = i + 1
+#    print(n)
+print("Select numbers: ", listNumb)
